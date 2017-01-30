8df46b76c00945d9be7e736e7ea5ad8166648df2
ybhartia <yash@bhartia.in>
2017-01-29 23:09:36 -0800

    Initial Commit

---
+++ MAPPING/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MAPPING
+//
+//  Created by yash bhartia on 1/29/17.
+//  Copyright © 2017 CreativeMedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ MAPPINGTests/MAPPINGTests.swift
@@ -0,0 +1,36 @@
+//
+//  MAPPINGTests.swift
+//  MAPPINGTests
+//
+//  Created by yash bhartia on 1/29/17.
+//  Copyright © 2017 CreativeMedia. All rights reserved.
+//
+
+import XCTest
+@testable import MAPPING
+
+class MAPPINGTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
